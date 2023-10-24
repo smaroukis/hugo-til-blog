90d2b62aeca4d226a7a9b80a1aac6866704deedd
Spencer <smaroukis@gmail.com>
2023-10-24 17:26:26 +0900

    reorg shell script

--- build/export-and-serve.sh
+++ build/export-and-serve.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 # running from "(proj root)/build" dir 
 EXPORT_BINARY="../mnt/obsidian-export/target/debug/obsidian-export"
@@ -12,41 +12,38 @@ function copy_vault {
     rm $VAULT_PATH/README.md
 }
 
+function prepare_hugo {
+    # save _index.md's
+    mkdir -p $HUGO_ROOT/content/tmp
+    cp -R $HUGO_ROOT/content/posts/_index.md $HUGO_ROOT/content/tmp
+    # clear content
+    rm -rf $HUGO_ROOT/content/posts
+    mkdir -p $HUGO_ROOT/content/posts
+    # add back in _index.md's
+    mv $HUGO_ROOT/content/tmp/* $HUGO_ROOT/content/posts/
+    rm -rf $HUGO_ROOT/content/tmp
+}
+
 echo "🍿 Preparing blog source / vault..."
 copy_vault
 
 echo "🍿 Preparing hugo content..."
-# save _index.md's
-mkdir -p $HUGO_ROOT/content/tmp
-cp -R $HUGO_ROOT/content/posts/_index.md $HUGO_ROOT/content/tmp
-# clear content
-rm -rf $HUGO_ROOT/content/posts
-mkdir -p $HUGO_ROOT/content/posts
-# add back in _index.md's
-mv $HUGO_ROOT/content/tmp/* $HUGO_ROOT/content/posts/
-rm -rf $HUGO_ROOT/content/tmp
+prepare_hugo
 
-echo "🍿 Exporting obsidian vault..."
+echo "🍿 Exporting obsidian vault with obsidian-export..."
 # Using dev version of obsidian-export
 $EXPORT_BINARY "$VAULT_PATH" --start-at "$VAULT_PATH" --frontmatter=always --link=none $HUGO_ROOT/content/posts/
 # added:   --link LINK-STRATEGY       Link strategy (one of: encoded, none) (default: encoded)
 
-# TODO refactor to seperate script for locally servinf
-# have switch statement for local or githuhbactions
-if [ "$GITHUB_ACTIONS" = "true" ]; then
-  # This is a GitHub Actions workflow
-  echo "🏗 Serving blog locally..."
+if [ "$GITHUB_ACTIONS" ]; then
+  # TODO may need to build into different dir, see script/publish setup
+  echo "Defer to Github Actions for building site within runner..."
+else
+  echo "🏗 Not a Github Action, Serving blog locally..."
   pushd $HUGO_ROOT > /dev/null
   hugo server -D
   popd > /dev/null
-else
-  # This is not a GitHub Actions workflow, so build the site
-  echo "Building the blog..."
-  pushd $HUGO_ROOT > /dev/null
-  hugo -D
-  popd > /dev/null
 fi
-
 
 # TODO - move $HUGO_ROOT/content/posts/attachments to $HUGO_ROOT/assets/images
 # TODO - add in script for responsive images
--- build/test.sh
+++ build/test.sh
@@ -5,3 +5,9 @@ EXPORT_BINARY="$LOCAL_REPO/mnt/obsidian-export/target/debug/obsidian-export"
 VAULT_PATH="$LOCAL_REPO/mnt/blog-source"
 HUGO_ROOT="$LOCAL_REPO/hugoroot"
 
+
+if [ "$GITHUB_ACTIONS" ]; then
+  echo "Building site for Github Actions..."
+else
+  echo "Not a Github Action, serving site locally..."
+fi
